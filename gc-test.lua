0e58b272b81cf33c70c11385d517052ce6f11e46
Markus Klotzbuecher <mk@marumbi.de>
2009-06-03 17:54:16 +0200

    Bugfix in timersub. Further parametrize the test.

--- gc-test.lua
+++ gc-test.lua
@@ -2,9 +2,18 @@
 require("rtposix")
 
 param = {}
-param.tabnum = 100
-param.tabsize = 100
+param.data_tabnum = 10
+param.data_tabsize = 10
+param.garbage_tabnum = 10
+param.garbage_tabsize = 10
 
+--param.type = "collect"
+param.type = "step"
+
+param.num_runs = 5000
+param.sleep_ns = 10000000 -- 10ms
+
+-- generate num garbage tables of tabsize
 function gen_garbage(num, tabsize)
    for n = 1,num do
       local t = {}
@@ -12,6 +21,18 @@ function gen_garbage(num, tabsize)
 	 table.insert(t,i)
       end
       t = {}
+   end
+end
+
+-- generate num data tables of tabsize
+data = {}
+function gen_data(num, tabsize)
+   for n = 1,num do
+      local t = {}
+      for i = 1,tabsize do
+	 table.insert(t,i)
+      end
+      table.insert(data, t)
    end
 end
 
@@ -23,7 +44,7 @@ function timersub(t1, t0)
       res.nsec = t1.nsec - t0.nsec
    else
       res.sec = t1.sec - t0.sec
-      res.nsec = t1.nsec + 1000000 - t0.sec
+      res.nsec = t1.nsec + 1000000 - t0.nsec
       
       if res.nsec >= 1000000 then
 	 res.sec = res.sec + 1
@@ -70,7 +91,9 @@ function timed_gc(type)
 
    -- is automatically restarted
    collectgarbage("stop")
+
    t1 = rtposix.gettime("MONOTONIC")
+
 
    stat.mem1 = collectgarbage("count")
    stat.dur = timersub(t1,t0)
@@ -78,54 +101,64 @@ function timed_gc(type)
    return stat
 end
 
-function stop_gc()
+function gc_stop()
    collectgarbage("stop")
    print("stopped GC")
 end
 
-function start_gc()
+function gc_start()
    print("starting GC")
    collectgarbage("start")
 end
 
-
--- helper
-function print_stats(s)
-   print("max duration: " .. tv2str(s.dur_max), "min duration: " .. tv2str(s.dur_min))
+function gc_setpause(val)
+   print("setting gcpause to ", val)
+   collectgarbage("setpause", val)
 end
+
+function gc_setstepmul(val)
+   print("setting setstepmul to ", val)
+   collectgarbage("setstepmul", val)
+end
+   
 
 -- initalize things
 stats = {}
 stats.dur_min = { sec=math.huge, nsec=math.huge }
 stats.dur_max = { sec=0, nsec=0 }
 
+-- initalize data
+gen_data(param.data_tabnum, param.garbage_tabsize)
+
 rtposix.mlockall("MCL_BOTH")
 rtposix.sched_setscheduler(0, "SCHED_FIFO", 99)
 
+gc_stop()
 
-stop_gc()
 print("doing initial full collect")
 print_gcstat(timed_gc("collect"))
 
-for i = 1,math.huge do
-   gen_garbage(param.tabnum, param.tabsize)
-   s = timed_gc("step")
+for i = 1,param.num_runs do
    
+   gen_garbage(param.garbage_tabnum, param.garbage_tabsize)
+
+   s = timed_gc(param.type)
+
    if timercmp(s.dur, stats.dur_min) < 0 then
       stats.dur_min = s.dur
-      print_stats(stats)
+      print_gcstat(s)
    end
 
    if timercmp(s.dur, stats.dur_max) > 0 then
       stats.dur_max = s.dur
-      print_stats(stats)
+      print_gcstat(s)
    end
 
-   rtposix.nanosleep("MONOTONIC", "rel", 0, 100000000)
+   rtposix.nanosleep("MONOTONIC", "rel", 0, param.sleep_ns)
 end
 
 print("Statistics")
-print_stats(stats)
+print("max duration: " .. tv2str(stats.dur_max), "min duration: " .. tv2str(stats.dur_min))
 
 print("doing final full collect")
 print_gcstat(timed_gc("collect"))
