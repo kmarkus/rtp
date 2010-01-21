38227e6ec651bfb8d039d14d3884361ac23a72aa
Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>
2010-01-21 18:03:09 +0100

    add create_bench gc benchmark function creator

--- luagc.lua
+++ luagc.lua
@@ -4,9 +4,7 @@
 require("time")
 require("rtposix")
 
-local collectgarbage = collectgarbage
-local time = time
-local rtposix = rtposix
+local collectgarbage, time, rtposix, assert, math, io = collectgarbage, time, rtposix, assert, math, io
 
 module("luagc")
 
@@ -33,6 +31,11 @@ end
 function step()
    collectgarbage("step")
    stop()
+end
+
+-- step the gc at maximum for us
+-- at least one run will be made
+function step_max_us(us)
 end
 
 function full()
@@ -68,4 +71,46 @@ function timed_gc(type)
    stat.dur = time.sub(t1,t0)
 
    return stat
+end
+
+--
+-- create a garbage collection test closure which will perform and
+-- collections and record the worst case timing behavior.
+-- parameters:
+-- 	call without arguments for performing a collection
+--	'get_results' returns a timed_gc stats table
+--	'print_results' does what it claims to
+--
+function create_bench(gctype)
+
+   assert(gctype == 'collect' or gctype == 'step',
+	  "create_bench: argument must be either 'collect' or 'step'")
+
+   local stats = {}
+   stats.dur_min = { sec=math.huge, nsec=math.huge }
+   stats.dur_max = { sec=0, nsec=0 }
+   stats.dur_avg = { sec=0, nsec=0 }
+   stats.cnt = 0
+
+   return function (cmd)
+	     if cmd == 'get_results' then
+		return stats
+	     elseif cmd == 'print_results' then
+		io.stderr:write("max: " .. time.ts2str(stats.dur_max),
+				", min: " .. time.ts2str(stats.dur_min),
+				", avg: " .. time.ts2str(time.div(stats.dur_avg, stats.cnt)) .. "\n")
+	     else
+		stats.cnt = stats.cnt+1
+		local s = timed_gc(type)
+		stats.dur_avg = time.add(stats.dur_avg, s.dur)
+
+		if time.cmp(s.dur, stats.dur_min) < 0 then
+		   stats.dur_min = s.dur
+		end
+
+		if time.cmp(s.dur, stats.dur_max) > 0 then
+		   stats.dur_max = s.dur
+		end
+	     end
+	  end
 end
