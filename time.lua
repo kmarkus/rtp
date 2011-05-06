94ee385cf15c54e1d5b589520a7b3c5f910c20d2
Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>
2011-05-06 15:07:31 +0200

    time.lua: add luadoc comments

--- time.lua
+++ time.lua
@@ -1,10 +1,13 @@
--- sys/time.h like operations
+--- sys/time.h like operations
 --
 -- take struct timespec tables with 'sec' and 'nsec' fields as input
 --
 
 module("time")
 
+--- Subtract a timespec from another and normalize
+-- @param a timespec to subtract from
+-- @param b timespec to subtract
 function sub(a, b)
    local res = {}
    res.sec = a.sec - b.sec
@@ -17,6 +20,9 @@ function sub(a, b)
    return res
 end
 
+--- Add a timespec from another and normalize
+-- @param a timespec a
+-- @param b timespec b
 function add(a, b)
    local res = {}
    res.sec = a.sec + b.sec
@@ -29,6 +35,9 @@ function add(a, b)
    return res
 end
 
+--- Divide a timespec inplace
+-- @param t timespec to divide
+-- @param d divisor
 function div(t, d)
    local res = {}
    res.sec = t.sec / d
@@ -36,6 +45,8 @@ function div(t, d)
    return res
 end
 
+--- Compare to timespecs
+-- @result return 1 if t1 is greater than t2, -1 if t1 is less than t2 and 0 if t1 and t2 are equl
 function cmp(t1, t2)
    if(t1.sec > t2.sec) then
       return 1
@@ -50,10 +61,15 @@ function cmp(t1, t2)
    end
 end
 
+--- Convert timespec to microseconds
+-- @param ts timespec
+-- @result number of microseconds
 function ts2us(ts)
    return ts.sec * 1000000 + ts.nsec / 1000
 end
 
+--- Convert a timespec to a string (in micro-seconds)
+--- for pretty printing purposes
 function ts2str(ts)
    return ts2us(ts) .. "us"
 end
